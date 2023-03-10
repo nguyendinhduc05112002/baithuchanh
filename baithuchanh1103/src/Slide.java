9128deb8b0e233a7847c17cb8ef1464413875193
nguyendinhduc05112002 <127472701+nguyendinhduc05112002@users.noreply.github.com>
2023-03-10 20:51:35 +0700

    Delete baithuchanh1103 directory

--- baithuchanh1103/src/Slide.java
+++
@@ -1,7 +0,0 @@
-import java.util.Scanner;
-
-public class Slide {
-    public static void main(String[] args) {
-        System.out.println("Chao anh Duc nhan ngay8/3 anh chuc em xinh gai hon nha tuan");
-    }
-}
