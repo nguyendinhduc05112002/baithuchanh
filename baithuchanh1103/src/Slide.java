7757d7223a9dda736c49310adc516783e4a276b6
unknown <ducn3211@gmail.com>
2023-03-10 20:46:35 +0700

    1103

---
+++ baithuchanh1103/src/Slide.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class Slide {
+    public static void main(String[] args) {
+        System.out.println("Chao anh Duc nhan ngay8/3 anh chuc em xinh gai hon nha tuan");
+    }
+}
